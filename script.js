059ee67ebf0b93fbcd80e0fa10a5d6a3e404d9a4
Shahzod <shahzodtashev49@gmail.com>
2023-04-21 18:10:18 +0500

    second commit

--- script.js
+++ script.js
@@ -67,12 +67,68 @@ const receiptOut = document.querySelector('.receipt__window-out');
 const payBtn = document.querySelector('.receipt__window-btn');
 const logo = document.querySelector('.header__timer-extra');
 
+let cart = [];
+let fullName = '';
+let fullPrice = 0;
+let fullKcall = 0;
+
 increaseLvl(0);
 
 btnPlusOrMinus.forEach((btn) => {
+
     btn.addEventListener('click', function() {
-        plusOrMinus(this)
+        plusOrMinus(this);
     })
+})
+
+checkExtraProduct.forEach(product => {
+
+    product.addEventListener('click', function() {
+        addExtraProduct(this);
+    })
+})
+
+addCart.addEventListener('click', () => {
+    
+    for(const key in product) {
+        let burger = product[key]
+
+        if(burger.amount > 0) {
+            
+            for(const newKey in burger) {
+                if(burger[newKey] === true) {
+                    burger.name += '\n' + extraProduct[newKey].name
+                }
+            }
+            cart.push(burger)
+        };
+    }
+    
+    cart.forEach(burger => {
+
+        fullName += '\n' + burger.name + '\n' + 'Колличество:' + burger.amount + '\n';
+        fullPrice += burger.SUMM;
+        fullKcall += burger.KCALL;
+    })
+
+    receipt.style.display = 'flex';
+    setTimeout(() => {
+
+        receipt.style.opacity = '1';
+        receiptWindow.style.top = '0';
+    }, 100)
+
+    receiptOut.innerHTML = `Ваш заказ: \n ${fullName} \nКаллорийность: ${fullKcall} \nОбщая сумма: ${fullPrice}сумм`;
+
+    const amount = document.querySelectorAll('.main__product-num');
+    const price = document.querySelectorAll('.main__product-price span');
+    const kcall = document.querySelectorAll('.main__product-kcall span');
+
+    for(let i = 0; i < amount.length; i++) {
+        amount[i].innerText = 0;
+        price[i].innerText = 0;
+        kcall[i].innerText = 0;
+    }
 })
 
 function plusOrMinus(element) {
@@ -86,22 +142,52 @@ function plusOrMinus(element) {
 
     if(element.dataset.symbol === '+') {
         productAmount++
+
     } else {
         productAmount > 0 ? productAmount-- : '';
     }
 
     product[parentId]['amount'] = productAmount;
+    
     output.innerText = productAmount;
     price.innerText = product[parentId].SUMM;
     kcall.innerText = product[parentId].KCALL;
 }
 
 function increaseLvl(lvl) {
+
     if(lvl === 100) return;
     
     lvl++;
     logo.innerHTML = lvl
-    let timeout = setTimeout(() => increaseLvl(lvl), 100)
-    // clearInterval(interval);
+
+    let timeout = setTimeout(() => increaseLvl(lvl), lvl + lvl / 4);
 }
 
+
+
+function addExtraProduct(secondProduct) {
+
+    const parentNode = secondProduct.closest('.main__product');
+    const parentId = parentNode.getAttribute('id');
+    const price = parentNode.querySelector('.main__product-price span');
+    const kcall = parentNode.querySelector('.main__product-kcall span');
+    const extraData = secondProduct.dataset.extra;
+
+    product[parentId][secondProduct.dataset.extra] = secondProduct.checked;
+
+    if(product[parentId][extraData]) {
+
+        product[parentId].price += extraProduct[extraData].price
+        product[parentId].kcall += extraProduct[extraData].price
+    } else {
+
+        product[parentId].price -= extraProduct[extraData].price
+        product[parentId].kcall -= extraProduct[extraData].price
+    }
+
+    price.innerHTML = product[parentId].SUMM;
+    kcall.innerHTML = product[parentId].KCALL;
+}
+
+payBtn.addEventListener('click', () => location.reload())
